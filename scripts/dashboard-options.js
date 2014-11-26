8f081e4e1e41c2cdd40f2b8e605f044c9d53d2a3
Marco Bettiolo <marco@bettiolo.it>
2014-11-26 18:22:53 +0000

    Getting rid of everything in dasboard options, not needed

--- scripts/dashboard-options.js
+++ scripts/dashboard-options.js
@@ -1,41 +1,7 @@
-/* global console */
-
 function DashboardOptions() {
-  this.dashboardSectionId = 'dashboard';
+
 }
 
 DashboardOptions.prototype.init = function () {
-  this._parsedQueryString = this._parseQueryString(location.search);
-  this.urls = this._getUrlsFromQueryString();
-  this.interval = this._getUpdateIntervalInSeconds();
-};
 
-DashboardOptions.prototype._parseQueryString = function (queryString) {
-  // Taken from http://stackoverflow.com/a/21152762/26754
-  var qd = {};
-  queryString.substr(1).split('&').forEach(function (item) {
-    (item.split('=')[0] in qd) ?
-      qd[item.split('=')[0]].push(item.split('=')[1]) : qd[item.split('=')[0]] = [item.split('=')[1],];
-  });
-  return qd;
-};
-
-DashboardOptions.prototype._getUrlsFromQueryString = function () {
-  var decodedUrls = [];
-  if (this._parsedQueryString.dashboard instanceof Array) {
-    for (var i = 0; i < this._parsedQueryString.dashboard.length; i++) {
-      decodedUrls[i] = decodeURIComponent(this._parsedQueryString.dashboard[i]);
-    }
-  }
-  console.log('Available dashboards (' + decodedUrls.length + '): ' + decodedUrls);
-  return decodedUrls;
-};
-
-DashboardOptions.prototype._getUpdateIntervalInSeconds = function () {
-  var updateIntervalInSeconds = parseInt(this._parsedQueryString.interval) || 0;
-  if (updateIntervalInSeconds < 3) {
-    updateIntervalInSeconds = 90;
-  }
-  console.log('Refresh interval: ' + updateIntervalInSeconds);
-  return updateIntervalInSeconds;
 };
