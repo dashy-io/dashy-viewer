46c37a54ccd10763371fe26c3e393f8394adc1b2
Marco Bettiolo <marco@bettiolo.it>
2014-11-26 16:11:38 +0000

    Now handling generation of UUIDs for dashboards

--- show-dashboard.sh
+++ show-dashboard.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
+if [ ! -f ~/.dashy ]; then
+  echo "Initialising ~/.dashy config..."
+  echo "?id=$(uuidgen)" > ~/.dashy
+else
+  echo "Using existing ~/.dashy config."
+fi
+
 DASHBOARD_FILE="file://$(cd $(dirname $0); pwd -P)/index.html$(cat ~/.dashy)"
+echo "Url: ${DASHBOARD_FILE}"
 
 if command -v chromium >/dev/null 2>&1; then
   echo "Running dashboard with chromium"
-  chromium --incognito --kiosk $DASHBOARD_FILE
+  chromium --incognito --kiosk ${DASHBOARD_FILE}
   exit 0
 fi
 
 if command -v chromium-browser >/dev/null 2>&1; then
   echo "Running dashboard with chromium-browser"
-  chromium-browser --incognito --kiosk $DASHBOARD_FILE
+  chromium-browser --incognito --kiosk ${DASHBOARD_FILE}
+  exit 0
+fi
+
+if command -v google-chrome-stable >/dev/null 2>&1; then
+  echo "Running dashboard with google-chrome-stable"
+  google-chrome-stable --incognito --kiosk ${DASHBOARD_FILE}
   exit 0
 fi
 
 if command -v midori >/dev/null 2>&1; then
   echo "Running dashboard with midori"
-  midori -e Fullscreen -a $DASHBOARD_FILE
+  midori -e Fullscreen -a ${DASHBOARD_FILE}
   exit 0
 fi
 
 if command -v epiphany-browser >/dev/null 2>&1; then
   echo "Running dashboard with epiphany-browser"
-  epiphany-browser -a --profile /tmp $DASHBOARD_FILE
+  epiphany-browser -a --profile /tmp ${DASHBOARD_FILE}
   exit 0
 fi
 
